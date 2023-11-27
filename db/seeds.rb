09ff8663db87961ea3ea350bad6066db24d2b7d8
Nagendra Yadav <kumarnagendra2460@gmail.com>
2023-11-27 17:56:54 +0530

    Student-controller and seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,40 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+
+# student = Student.create(
+# 	first_name: 'Nagednra',
+# 	last_name: 'Yadav',
+# 	email: 'xyz@gmail.com'
+# 	)
+
+# (1..10).each do |i|
+#   Student.create(
+#     first_name: "pink#{i}",
+#     last_name: "show#{i}",
+#     email: "abc#{i}@example.com"
+    
+#   )
+# end
+
+# 20.times do |i|
+# 	Student.create(
+# 		first_name: "Student#{i}",
+# 		last_name: "Lname#{i}",
+# 		email: "student#{i}@gmail.com"
+# 	)
+# end
+
+
+# require 'faker'
+
+
+# (1..10).each do |i|
+# 	Student.create(
+# 	first_name: Faker::Internet.first_name,
+# 	last_name: Faker::Internet.last_name,
+# 	email: Faker::Internet.email
+# 	)
+# end
+
